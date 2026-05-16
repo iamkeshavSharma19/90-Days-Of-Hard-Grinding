f7a0a5f748bfd896f9d6f7e1af72519a4bb0c8e5
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-05-16 19:57:54 +0530

    initial commit

--- first.java
+++ first.java
@@ -1,5 +1,20 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("Namaste Keshav");
+        System.out.println("Hello World!");
+        System.out.println("Namaste Java 🚀");
+
+        int num = 6;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "x" + i + " = " + (num * i));
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("I Love Java ❤️❤️");
+        }
+
+        if (10 == 10) {
+            System.out.println("10");
+        }
     }
 }
