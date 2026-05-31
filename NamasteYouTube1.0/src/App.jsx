41a0a8b4609bfed8ed1645f0018b54f066113742
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-05-31 14:18:27 +0530

    Kick Starting Namaste YouTube 1.0 React Project

---
+++ NamasteYouTube1.0/src/App.jsx
@@ -0,0 +1,12 @@
+import Head from "./components/Head";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Namaste Youtube</h1>
+      <Head />
+    </div>
+  );
+}
+
+export default App;
---
+++ NamasteYouTube1.0/src/components/Head.jsx
@@ -0,0 +1,9 @@
+function Head() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl">Head</h1>
+    </>
+  );
+}
+
+export default Head;
