35236e5c2a53f31cc38bc7222545835e412a2f63
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-06-09 16:24:20 +0530

    initial commit

--- Nodejs02/DevTinderPractice/src/app.js
+++ Nodejs02/DevTinderPractice/src/app.js
@@ -6,19 +6,8 @@ import { connectDB } from "./config/database.js";
 const app = express();
 const PORT = process.env.PORT || 7778;
 
-console.log(process.env.MONGODB_URI);
-console.log(process.env.PORT);
-
-const returnedValue = connectDB();
-
-console.log(returnedValue);
-
-console.log(returnedValue);
-
 connectDB()
   .then((res) => {
-    console.log(res); //mongoose object
-
     console.log("Database connection established");
 
     app.listen(PORT, (err) => {
--- Nodejs02/DevTinderPractice/src/config/database.js
+++ Nodejs02/DevTinderPractice/src/config/database.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const promiseResult = await mongoose.connect(process.env.MONGODB_URI); //mongoose object
-    console.log("Checking Async And Await");
-    console.log(promiseResult);
+
     return promiseResult;
   } catch (error) {
     console.log(error);
   }
 };
-
