cb9f4d2e35ecc246854ca1de5e02b1733dfedd36
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-06-22 22:58:25 +0530

    initial Commit

---
+++ NamasteYouTube1.0/src/Notes.js
@@ -0,0 +1,9 @@
+//&Suppose In the search bar if i am searching for India,so it starts showing you the suggestions about the India.It basically makes an API call and shows you the sugggesions.
+
+//?Basically when I start typing in "India", so basically It is calling this search API.You Can also filter this out with the help of the Search API.
+
+//^When I am typing "India",it is making an API Call in each and every key Press.In each and every key Press it is making an API Call.What if I tell you that this is not the case.
+
+//*Suppose If I was about to search for "flag" right,but just for flag,I basically made 4 API calls.The first 3 calls are almost waste for me.Now I will type in very fast on the youtube.Now see How many API Calls it will make if I type in very fast.I typed 7 letters very fast but it only made 3 API calls by the way.It made 3 API calls for the 7 Key Strokes.
+
+//~Now If I try to search "clothes" on the flipkart website,Let us see What will we get.It is not making an API call on the every Key Press.Only make an API call once I have finished writing something.Why to make API calls on the every key stroke.This concept is Known as Debouncing.Suppose I am typing very very fast.
